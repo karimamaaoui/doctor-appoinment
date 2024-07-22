4198fb50d0a893a4aba4db1235de903adf041602
maaoui karima <maaouika07@gmail.com>
2024-07-22 14:17:01 +0100

    cors config

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,5 +1,5 @@
 // domain name : give access to frontend with port 5173 to connect to the server
-const allowedOrigins=["http://localhost:56046"]
+const allowedOrigins=["http://localhost:4200"]
     
 
 module.exports= allowedOrigins;
